ffb0e1cc6c0712b9ef38b949121463f7b41099b4
Adilkhan <adilkhankenzhetaev@gmail.com>
2023-07-12 11:48:43 +0600

    chore: remove UMA

--- Assets/UMA/Core/Decals/Scripts/DecalManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DecalManager : MonoBehaviour
-{
-    public List<DecalDefinition> Decals = new List<DecalDefinition>();
-}

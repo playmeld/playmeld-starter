05cc62d2a5549b2e138ace7417d24aefda59085e
Adilkhan <adilkhankenzhetaev@gmail.com>
2023-07-12 11:50:12 +0600

    chore: introduce UMA

---
+++ Assets/UMA/Core/Decals/Scripts/DecalManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalManager : MonoBehaviour
+{
+    public List<DecalDefinition> Decals = new List<DecalDefinition>();
+}
